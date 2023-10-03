20b03d4cb03d232afc509fd2beb7f4f6643e33cf
amusukwa <amusukwa@gmail.com>
2023-10-03 04:30:01 -0700

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-## Check if an argument is provided
+
+# Check if an argument is provided
 if ARGV.empty?
   puts "Usage: #{$PROGRAM_NAME} <string>"
   exit 1
@@ -11,8 +12,9 @@ input_string = ARGV[0]
 # Define the regular expression
 pattern = /School/
 
-# Match the input string against the regular expression
-matches = input_string.scan(pattern)
-
-# Print the matches with the desired format
-puts matches.join("$")
+# Check if the input string matches the regular expression
+if input_string.match?(pattern)
+  puts "#{input_string}$"
+else
+  puts "No match"
+end
