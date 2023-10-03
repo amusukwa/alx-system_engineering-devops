042a4445828976d75036b2f499b23f5678473b8f
amusukwa <amusukwa@gmail.com>
2023-10-03 04:34:16 -0700

    corrections

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <string>"
-  exit 1
-end
-
-# Get the argument
-input_string = ARGV[0]
-
-# Define the regular expression
-pattern = /School/
-
-# Check if the input string matches the regular expression
-if input_string.match?(pattern)
-  puts "#{input_string}$"
-else
-  puts "No match"
-end
+puts ARGV[0].scan(/School/).join
