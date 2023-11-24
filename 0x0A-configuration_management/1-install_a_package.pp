5f60cc9311f20c7abd5b0d79beaa53f1d9925c61
amusukwa <amusukwa@example.com>
2023-11-24 05:43:55 -0800

    1-install_a_package.pp 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,9 @@
 #!/usr/bin/pup
 #install flask from pip3
 
-package { 'flask':
-  ensure     => '2.1.0',
-  provider   => 'pip3'
-  
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+  creates     => '/usr/local/lib/python3.x/dist-packages/Flask-2.1.0.dist-info',
 }
