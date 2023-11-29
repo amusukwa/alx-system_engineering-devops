94ac09b9d5851d7f186843a2a59476ae642b21e1
amusukwa <amusukwa@example.com>
2023-11-29 07:22:10 -0800

    7-puppet_install_nginx_web_server.pp 2

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,41 @@
 # 5-install_nginx_redirect_301.pp
 
 # Install Nginx package
+exec { 'update':
+  command => 'app-get -y update',
+  path => '/usr/bin',
+}
+
 package { 'nginx':
-  ensure => 'installed',
-}
+  ensure => installed,
+  name => 'nginx',
+  provider => 'apt',
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-
-}
-
-file { '/var/www/html/404.html':
-  content => "Ceci n'est pas une page",
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => template('/etc/nginx/nginx.conf'),
-  notify  => Service['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure => 'link',
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
 }
 
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+  ensure => running,
+  enable => true,
+  require => Package['nginx'],
+}
+
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "
+  server {
+   listen 80;
+   location / {
+    return 200 'Hello World':
+   }
+   location /redirect_me {
+    return 301 http://\$host
+   }",
+  notify => Service['nginx'],
+}
+
+exec { 'nginx_reload':
+  command => '/usr/sbin/service nginx reload',
+  refreshonly => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
