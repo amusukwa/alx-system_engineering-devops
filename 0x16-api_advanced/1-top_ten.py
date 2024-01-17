efae0e765fc17e10394bb6ea7c1b669fdfcd6fba
amusukwa <amusukwa@gmail.com>
2024-01-17 12:07:57 -0800

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,8 +9,7 @@ def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = 'https://www.reddit.com/r/programming/hot.json?limit=10'
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
-                      '(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Custom'
     }
 
     response = requests.get(
